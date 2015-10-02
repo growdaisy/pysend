c46da7419dee72f543ea289eb6b6531dfa31a280
Pokey Rule <pokey.rule@gmail.com>
2015-10-02 13:07:40 -0700

    Update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='pysend',
 
-    version='0.0.1',
+    version='0.0.2',
 
     description='Unified interface for sending email',
     long_description=long_description,
